8151b4a87f906fd856fd1c113c48392d542d0994
Philippe Rémy <premy@cogent.co.jp>
2018-05-27 19:40:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keras-tcn',
-    version='1.3.0',
+    version='1.4.0',
     description='Keras TCN',
     author='Philippe Remy',
     license='MIT',
