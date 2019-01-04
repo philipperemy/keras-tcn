bcc210aab8eb3784821ebe620ee6a40e06f50101
philipperemy <premy@cogent.co.jp>
2019-01-04 14:20:09 +0900

    2.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keras-tcn',
-    version='2.3.3',
+    version='2.3.4',
     description='Keras TCN',
     author='Philippe Remy',
     license='MIT',
