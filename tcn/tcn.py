be14b9846415063815499755666b813112c90e93
Psomers3 <37300408+psomers3@users.noreply.github.com>
2019-10-10 11:57:51 +0200

    made activation statically relu

--- tcn/tcn.py
+++ tcn/tcn.py
@@ -83,7 +83,7 @@ class ResidualBlock(Layer):
                     # TODO should be WeightNorm here, but using batchNorm instead
                     self._add_and_activate_layer(BatchNormalization())
 
-                self._add_and_activate_layer(Activation(self.activation))
+                self._add_and_activate_layer(Activation('relu'))
                 self._add_and_activate_layer(SpatialDropout1D(rate=self.dropout_rate))
 
             # 1x1 conv to match the shapes (channel dimension).
