9039cae38accce9646146c3982bb6e162703895c
Philippe Remy <premy.enseirb@gmail.com>
2020-01-25 16:53:23 +0900

    minor

--- tasks/visualise_activations.py
+++ tasks/visualise_activations.py
@@ -4,6 +4,9 @@ Output after 1 epochs on CPU: ~0.8611
 Time per epoch on CPU (Core i7): ~64s.
 Based on: https://github.com/keras-team/keras/blob/master/examples/imdb_bidirectional_lstm.py
 """
+import os
+import shutil
+
 import keract  # pip install keract
 import keras
 import matplotlib.pyplot as plt
@@ -16,6 +19,22 @@ from tensorflow.keras.preprocessing import sequence
 from tcn import TCN
 
 index_from_ = 3
+
+
+def visualize(model, x, max_len, tcn_num_filters, tcn_layer_outputs, prefix):
+    for i in range(len(x)):
+        tcn_outputs = keract.get_activations(model, x[i:i + 1], nodes_to_evaluate=tcn_layer_outputs)
+        tcn_blocks_outputs = [v for (k, v) in tcn_outputs.items() if v.shape == (1, max_len, tcn_num_filters)]
+        plt.figure(figsize=(10, 2))  # creates a figure 10 inches by 10 inches
+        plt.title('TCN internal outputs (one row = one residual block output)')
+        plt.xlabel('Timesteps')
+        plt.ylabel('Forward pass\n (top to bottom)')
+        plt.imshow(np.max(np.vstack(tcn_blocks_outputs), axis=-1), cmap='jet', interpolation='bilinear')
+        plt.savefig(f'acts/{prefix}_{i}.png', dpi=1000, bbox_inches='tight', pad_inches=0)
+        plt.show()
+        plt.clf()
+        plt.cla()
+        plt.close()
 
 
 def get_word_mappings():
@@ -39,120 +58,94 @@ def print_text(x_):
     print(' '.join(id_to_word[ii] for ii in x_))
 
 
-max_features = 20000
-# cut texts after this number of words
-# (among top max_features most common words)
-max_len = 100
-batch_size = 32
-tcn_num_filters = 10
+def main():
+    max_features = 20000
+    # cut texts after this number of words
+    # (among top max_features most common words)
+    max_len = 100
+    batch_size = 32
+    tcn_num_filters = 10
 
-print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features, index_from=index_from_)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+    print('Loading data...')
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features, index_from=index_from_)
+    print(len(x_train), 'train sequences')
+    print(len(x_test), 'test sequences')
 
-naoko = "Put all speaking her delicate recurred possible. " \
-        "Set indulgence discretion insensible bed why announcing. " \
-        "Middleton fat two satisfied additions. " \
-        "So continued he or commanded household smallness delivered. " \
-        "Door poor on do walk in half. " \
-        "Roof his head the what. " \
-        "Society excited by cottage private an it seems. " \
-        "Fully begin on by wound an. " \
-        "The movie was very good. I highly recommend. " \
-        "At declared in as rejoiced of together. " \
-        "He impression collecting delightful unpleasant by prosperous as on. " \
-        "End too talent she object mrs wanted remove giving. " \
-        "Man request adapted spirits set pressed. " \
-        "Up to denoting subjects sensible feelings it indulged directly."
+    x_val = [
+        encode_text('The movie was very good. I highly recommend.'),  # will be at the end.
+        encode_text(' '.join(["worst"] * 100)),
+        encode_text("Put all speaking her delicate recurred possible. "
+                    "Set indulgence discretion insensible bed why announcing. "
+                    "Middleton fat two satisfied additions. "
+                    "So continued he or commanded household smallness delivered. "
+                    "Door poor on do walk in half. "
+                    "Roof his head the what. "
+                    "Society excited by cottage private an it seems. "
+                    "Fully begin on by wound an. "
+                    "The movie was very good. I highly recommend. "
+                    "At declared in as rejoiced of together. "
+                    "He impression collecting delightful unpleasant by prosperous as on. "
+                    "End too talent she object mrs wanted remove giving. "
+                    "Man request adapted spirits set pressed. "
+                    "Up to denoting subjects sensible feelings it indulged directly.")
+    ]
 
-x_val = [encode_text('The movie was very good. I highly recommend.'),  # will be at the end.
-         encode_text(' '.join('The movie was the worst movie I have ever '
-                              'seen in my life'.split(' ') + 86 * ['the'])),
-         encode_text("It doesn't do anything new or even terribly distinctive but maybe it "
-                     "didn't have to. It just had to be good enough to stick the landing "
-                     "and it does that."),
-         encode_text(' '.join(["worst"] * 100)),
-         encode_text(naoko)
-         ]
+    y_val = [1, 0, 1]
 
-y_val = [1, 0, 1, 0, 1]
+    print('Pad sequences (samples x time)')
+    x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+    x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+    x_val = sequence.pad_sequences(x_val, maxlen=max_len)
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+    print('x_val shape:', x_val.shape)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+    y_val = np.array(y_val)
 
-# print_text(index_from, x_test[0])
-# print_text(index_from, x_test[1])
-# print_text(index_from, x_test[2])
-# print_text(index_from, x_test[3])
-# print_text(index_from, x_test[4])
+    x_val[x_val > max_features] = 2  # oov.
 
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=max_len)
-x_test = sequence.pad_sequences(x_test, maxlen=max_len)
-x_val = sequence.pad_sequences(x_val, maxlen=max_len)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-print('x_val shape:', x_val.shape)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-y_val = np.array(y_val)
+    for i in range(10):
+        print(f'x_test[{i}]=', end=' | ')
+        print_text(x_test[i])
 
-x_val[x_val > max_features] = 2  # oov.
+    for i in range(len(x_val)):
+        print(f'x_val[{i}]=', end=' | ')
+        print_text(x_val[i])
 
-for i in range(10):
-    print_text(x_test[i])
+    temporal_conv_net = TCN(
+        nb_filters=tcn_num_filters,
+        kernel_size=7,
+        dilations=[1, 2, 4, 8, 16, 32]
+    )
 
-for i in range(len(x_val)):
-    print_text(x_val[i])
+    print(temporal_conv_net.receptive_field)
 
-temporal_conv_net = TCN(
-    nb_filters=tcn_num_filters,
-    kernel_size=7,
-    dilations=[1, 2, 4, 8, 16]
-)
+    model = Sequential()
+    model.add(Embedding(max_features, 128, input_shape=(max_len,)))
+    model.add(temporal_conv_net)
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-print(temporal_conv_net.receptive_field)
+    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
 
-model = Sequential()
-model.add(Embedding(max_features, 128, input_shape=(max_len,)))
-model.add(temporal_conv_net)
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+    tcn_layer_outputs = list(temporal_conv_net.layers_outputs)
 
-model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=4,
+              validation_data=[x_test, y_test])
 
-tcn_layer_outputs = list(temporal_conv_net.layers_outputs)
+    if os.path.exists('acts'):
+        shutil.rmtree('acts')
+    os.makedirs('acts')
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=4,
-          validation_data=[x_test, y_test])
+    print(model.predict_on_batch(x_val))
+    print(y_val)
 
-# example 9.png is very interesting.
-for i in range(10):
-    tcn_outputs = keract.get_activations(model, x_test[i:i + 1], nodes_to_evaluate=tcn_layer_outputs)
-    tcn_blocks_outputs = [v for (k, v) in tcn_outputs.items() if v.shape == (1, max_len, tcn_num_filters)]
-    plt.figure(figsize=(10, 2))  # creates a figure 10 inches by 10 inches
-    plt.title('TCN internal outputs (one row = one residual block output)')
-    plt.xlabel('Timesteps')
-    plt.ylabel('Forward pass\n (top to bottom)')
-    plt.imshow(np.max(np.vstack(tcn_blocks_outputs), axis=-1), cmap='jet', interpolation='bilinear')
-    plt.savefig(f'x_test_example_{i}.png', dpi=1000, bbox_inches='tight', pad_inches=0)
-    plt.show()
-    plt.clf()
-    plt.cla()
-    plt.close()
+    visualize(model, x_test[0:10], max_len, tcn_num_filters, tcn_layer_outputs, 'x_test')
+    visualize(model, x_val, max_len, tcn_num_filters, tcn_layer_outputs, 'x_val')
 
-print(model.predict_on_batch(x_val))
 
-for i in range(len(x_val)):
-    tcn_outputs = keract.get_activations(model, x_val[i:i + 1], nodes_to_evaluate=tcn_layer_outputs)
-    tcn_blocks_outputs = [v for (k, v) in tcn_outputs.items() if v.shape == (1, max_len, tcn_num_filters)]
-    plt.figure(figsize=(10, 2))  # creates a figure 10 inches by 10 inches
-    plt.title('TCN internal outputs (one row = one residual block output)')
-    plt.xlabel('Timesteps')
-    plt.ylabel('Forward pass\n (top to bottom)')
-    plt.imshow(np.max(np.vstack(tcn_blocks_outputs), axis=-1), cmap='jet', interpolation='bilinear')
-    plt.savefig(f'x_val_example_{i}.png', dpi=1000, bbox_inches='tight', pad_inches=0)
-    plt.show()
-    plt.clf()
-    plt.cla()
-    plt.close()
+if __name__ == '__main__':
+    main()
