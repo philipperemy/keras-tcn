03277bb47ca7e8b699f7e2bb469f245b0a79ce45
Philippe Remy <premy.enseirb@gmail.com>
2020-01-25 13:50:43 +0900

    remove unused file

--- ddd.py
+++
@@ -1,4 +0,0 @@
-a = "pfeiffer explore the darker side of double identities br br there are some intriguing concepts in this film about the only weakness i can really point out is a certain to the script in some places which i think is due mostly to the way this film is a four cornered fight there simply isn't enough time to properly explore what's going on br br nevertheless this is a damn good film i highly recommend watching this in <UNK> with the first and then weeping for how good the series could have been had it continued under burton and keaton"
-b = a.split(' ')
-
-print(' '.join(b[70:85]))
