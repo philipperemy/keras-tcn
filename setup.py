94e94705577181191d6de14c8c99c169ae3e353c
Philippe Remy <premy.enseirb@gmail.com>
2020-04-25 19:18:46 +0900

    3.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keras-tcn',
-    version='3.0.2',
+    version='3.1.0',
     description='Keras TCN',
     author='Philippe Remy',
     license='MIT',
