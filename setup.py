a13b0b5dc7cfb4f2d3ac11437f168741507dcfe0
Philippe Remy <premy.enseirb@gmail.com>
2020-06-05 09:12:34 +0900

    upgrade to TF22

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     # manually install tensorflow or tensorflow-gpu
     install_requires=[
         'numpy>=1.18.1',
-        'keras>=2.3.1',
-        'gast==0.2.2'
+        'keras==2.3.1',
     ]
 )
