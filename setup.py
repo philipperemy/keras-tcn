bdf4241b9d5ef5e08bfc156a42dd761f9b7a34d7
Philippe Remy <premy.enseirb@gmail.com>
2021-02-16 18:53:59 +0900

    3.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keras-tcn',
-    version='3.2.1',
+    version='3.3.0',
     description='Keras TCN',
     author='Philippe Remy',
     license='MIT',
--- tcn/__init__.py
+++ tcn/__init__.py
@@ -1,3 +1,3 @@
 from tcn.tcn import TCN, compiled_tcn, tcn_full_summary  # noqa
 
-__version__ = '3.2.1'
+__version__ = '3.3.0'
