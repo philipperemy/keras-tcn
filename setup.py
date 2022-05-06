ffdf1aa8dc0f9dcd29f82d5c5e9165fa747d5848
philipperemy <premy.enseirb@gmail.com>
2022-05-06 20:18:52 +0900

    macOs

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+import platform
+
 from setuptools import setup
+import os
+
+tensorflow = 'tensorflow'
+if platform.system() == 'Darwin' and platform.processor() == 'arm':
+    tensorflow = 'tensorflow-macos'
+    # https://github.com/grpc/grpc/issues/25082
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_OPENSSL'] = '1'
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_ZLIB'] = '1'
+
+install_requires = ['numpy', tensorflow, 'tensorflow_addons']
 
 setup(
     name='keras-tcn',
@@ -9,7 +21,5 @@ setup(
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     packages=['tcn'],
-    install_requires=[
-        'numpy', 'tensorflow', 'tensorflow_addons'
-    ]
+    install_requires=install_requires
 )
