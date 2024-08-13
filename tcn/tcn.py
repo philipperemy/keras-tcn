1f4355cfd89f28b9e985a59fb0862b2528d9cc95
philipperemy <premy.enseirb@gmail.com>
2024-08-13 11:54:04 -0700

    Fix bug introduced in Keras 3

--- tcn/tcn.py
+++ tcn/tcn.py
@@ -315,7 +315,10 @@ class TCN(Layer):
         else:
             self.output_slice_index = -1  # causal case.
         self.slicer_layer = Lambda(lambda tt: tt[:, self.output_slice_index, :], name='Slice_Output')
-        self.slicer_layer.build(self.build_output_shape.as_list())
+        try:
+            self.slicer_layer.build(self.build_output_shape.as_list())
+        except AttributeError:
+            self.slicer_layer.build(self.build_output_shape)
 
     def compute_output_shape(self, input_shape):
         """
